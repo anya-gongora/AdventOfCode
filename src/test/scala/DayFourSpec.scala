d69e0ed4c9d71adf3fd602ea06ef66b84ed1f1b3
Anya Gongora <anyagongora@gmail.com>
2017-12-05 10:07:00 -0400

    Add DayFourSpec

--- src/test/scala/DayFourSpec.scala
+++ src/test/scala/DayFourSpec.scala
@@ -1,3 +1,24 @@
-class DayFourSpec {
+import org.scalatest.{FlatSpec, Matchers}
 
+class DayFourSpec extends FlatSpec with Matchers {
+
+  "DayFour.countValidPassPhrases" should "return 3" in {
+    val lines =
+      "aa bb cc dd ee\n aa bb cc dd aa\n aa bb cc dd aaa\n aaa bbb cc dd a\n aaa bb cc ddd aaa"
+
+    val listOfLines = lines.split("\n").toSeq
+
+    val count = DayFour.countValidPassPhrases(listOfLines)
+    count shouldEqual 3
+  }
+
+  "DayFour.countUniquePassphrases" should "return 2" in {
+    val lines =
+      "abcde fghij\nabcde xyz ecdaba\niiii oiii ooii oooi oooo\noiii ioii iioi iiio"
+
+    val listOfLines = lines.split("\n").toSeq
+
+    val count = DayFour.countUniquePassphrases(listOfLines)
+    count shouldEqual 2
+  }
 }
